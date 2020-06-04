2497a4ce9e4fd32f8da94170589f2a24580ccd02
Apoorva Dhanala <dhanalapoorva18@gmail.com>
2020-06-04 19:04:26 +0000

    Store comments in Datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,6 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -41,11 +44,13 @@ private static final ArrayList<String> messagesList = new ArrayList<String>();
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String text = getParameter(request, "text-input", "");
-    messagesList.add(text);
+    // messagesList.add(text);
 
-    // Respond with the result.
-    response.setContentType("text/html;");
-    response.getWriter().println(Arrays.asList(messagesList));
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("content", text);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
 
     // Redirect back to the HTML page.
     response.sendRedirect("/index.html");
